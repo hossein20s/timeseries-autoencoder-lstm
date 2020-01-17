22cbe1e9a1c8e47d8678805e9fd2dd9cd5154f4f
Hossein Aklaghpour <hossein.akhlaghpour@auto-grid.com>
2020-01-17 02:28:15 +0000

    checkpoints

--- LSTNet/main.py
+++ LSTNet/main.py
@@ -5,10 +5,13 @@
 ####################################################################################
 
 # This must be set in the beggining because in model_util, we import it
+
+
 logger_name = "lstnet"
 
 # Path appended in order to import from util
 import sys
+
 sys.path.append('..')
 from util.model_util import LoadModel, SaveModel, SaveResults, SaveHistory
 from util.Msglog import LogInit
@@ -22,29 +25,53 @@ from lstnet_plot import AutoCorrelationPlot, PlotHistory, PlotPrediction
 
 import tensorflow as tf
 
-
 custom_objects = {
-        'PreSkipTrans': PreSkipTrans,
-        'PostSkipTrans': PostSkipTrans,
-        'PreARTrans': PreARTrans,
-        'PostARTrans': PostARTrans
-        }
+    'PreSkipTrans': PreSkipTrans,
+    'PostSkipTrans': PostSkipTrans,
+    'PreARTrans': PreARTrans,
+    'PostARTrans': PostARTrans
+}
 
-def train(model, data, init, tensorboard = None):
-    if init.validate == True:
+
+def train_and_checkpoint(model, data, init, tensorboard=None):
+    if init.validate:
         val_data = (data.valid[0], data.valid[1])
     else:
         val_data = None
 
+    checkpoint = tf.train.Checkpoint(step=tf.Variable(1), optimizer=lstnet.optimizer, net=lstnet)
+    manager = tf.train.CheckpointManager(checkpoint, './save', max_to_keep=3)
+    checkpoint.restore(manager.latest_checkpoint)
+    if manager.latest_checkpoint:
+        log.info("Restored from {}".format(manager.latest_checkpoint))
+        model.load_weights(manager.latest_checkpoint)
+        # model.build(tf.TensorShape([1, None]))
+    else:
+        log.info("Initializing from scratch.")
+
+    csv_logger = tf.keras.callbacks.CSVLogger('log/training.log', )
+    callbacks = [csv_logger]
+    if tensorboard:
+        callbacks.append(lstnet_tensorboard)
+
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath='save/cp.ckpt',
+                                                             save_weights_only=True,
+                                                             save_best_only=True,
+                                                             monitor='val_accuracy',
+                                                             mode='max',
+                                                             verbose=1)
+
+    callbacks.append(checkpoint_callback)
+
     start_time = datetime.now()
     history = model.fit(
-                x = data.train[0],
-                y = data.train[1],
-                epochs = init.epochs,
-                batch_size = init.batchsize,
-                validation_data = val_data,
-                callbacks = [tensorboard] if tensorboard else None
-            )
+        x=data.train[0],
+        y=data.train[1],
+        epochs=init.epochs,
+        batch_size=init.batchsize,
+        validation_data=val_data,
+        callbacks=callbacks
+    )
     end_time = datetime.now()
     log.info("Training time took: %s", str(end_time - start_time))
 
@@ -62,7 +89,7 @@ if __name__ == '__main__':
 
     # Initialise parameters
     lstnet_init = LSTNetInit(args)
-    
+
     # Initialise logging
     log = LogInit(logger_name, lstnet_init.logfilename, lstnet_init.debuglevel, lstnet_init.log)
     log.info("Python version: %s", sys.version)
@@ -79,7 +106,7 @@ if __name__ == '__main__':
                     lstnet_init.horizon,
                     lstnet_init.window,
                     lstnet_init.normalise)
-    
+
     # If file does not exist, then Data will not have attribute 'data'
     if hasattr(Data, 'data') is False:
         log.critical("Could not load data!! Exiting")
@@ -113,9 +140,10 @@ if __name__ == '__main__':
 
     # Model Training
     if lstnet_init.train is True:
+
         # Train the model
         log.info("Training model ... ")
-        h = train(lstnet, Data, lstnet_init, lstnet_tensorboard)
+        h = train_and_checkpoint(lstnet, Data, lstnet_init, lstnet_tensorboard)
 
         # Plot training metrics
         if lstnet_init.plot is True:
@@ -124,16 +152,16 @@ if __name__ == '__main__':
         # Saving model if lstnet_init.save is not None.
         # There's no reason to save a model if lstnet_init.train == False
         SaveModel(lstnet, lstnet_init.save)
-        if lstnet_init.saveresults == True:
+        if lstnet_init.saveresults:
             SaveResults(lstnet, lstnet_init, h.history, test_result, ['loss', 'rse', 'corr'])
-        if lstnet_init.savehistory == True:
+        if lstnet_init.savehistory:
             SaveHistory(lstnet_init.save, h.history)
 
     # Validation
-    if lstnet_init.train is False and lstnet_init.validate is True:
+    if not lstnet_init.train and lstnet_init.validate:
         loss, rse, corr = lstnet.evaluate(Data.valid[0], Data.valid[1])
         log.info("Validation on the validation set returned: Loss:%f, RSE:%f, Correlation:%f", loss, rse, corr)
-    elif lstnet_init.validate == True:
+    elif lstnet_init.validate:
         log.info("Validation on the validation set returned: Loss:%f, RSE:%f, Correlation:%f",
                  h.history['val_loss'][-1], h.history['val_rse'][-1], h.history['val_corr'][-1])
 
